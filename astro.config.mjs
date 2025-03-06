0912e8a01dff10352fa8816886063027ac9b09fe
mystica2000 <mysticainf@gmail.com>
2025-03-07 02:47:22 +0530

    revert back font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 import fs from "node:fs";
-import { exportAsPng } from './ogImage';
-import path from 'node:path';
+import { exportAsPng } from "./ogImage";
+import path from "node:path";
 import { platform } from "node:os";
 
 import sitemap from "@astrojs/sitemap";
@@ -11,19 +11,16 @@ import sitemap from "@astrojs/sitemap";
 const og = () => ({
   name: "satori-og",
   hooks: {
-
     // credits to https://dietcode.io/p/astro-og/ for building og image at build time
     "astro:build:done": async ({ dir, pages }) => {
-
-      const interFont = fs.readFileSync(
-        "public/fonts/inter.ttf"
-      );
+      const interFont = fs.readFileSync("public/fonts/inter.ttf");
       const isPlatformWindows = platform() == "win32" ? true : false;
 
       for (const { pathname } of pages) {
-
-        if (/(^(posts)\/[a-zA-Z0-9-])/.test(pathname) || /(^(bytes)\/[a-zA-Z0-9-])/.test(pathname)) {
-
+        if (
+          /(^(posts)\/[a-zA-Z0-9-])/.test(pathname) ||
+          /(^(bytes)\/[a-zA-Z0-9-])/.test(pathname)
+        ) {
           if (pathname.startsWith("posts/")) {
             const filename = pathname.slice(6, -1);
             const file = fs.readFileSync(`src/content/posts/${filename}.mdx`);
@@ -33,47 +30,44 @@ const og = () => ({
               `${isPlatformWindows == true ? dir.pathname.slice(1) : dir.pathname}${pathname}og.png`,
               svg
             );
-
           } else {
-
             const filename = pathname.slice(6, -1);
-            const file = await fs.readFileSync(`src/content/bytes/${filename}.mdx`);
+            const file = fs.readFileSync(`src/content/bytes/${filename}.mdx`);
             const svg = await exportAsPng(file, interFont);
-            const pathFile = path.join(`${isPlatformWindows == true ? dir.pathname.slice(1) : dir.pathname}`, pathname, `og.png`);
-            fs.writeFileSync(
-              pathFile,
-              svg
+            const pathFile = path.join(
+              `${isPlatformWindows == true ? dir.pathname.slice(1) : dir.pathname}`,
+              pathname,
+              `og.png`
             );
+            fs.writeFileSync(pathFile, svg);
           }
         } else if (pathname == "") {
-          const file = await fs.readFileSync(`src/assets/images/og.png`);
-          const pathFile = path.join(`${isPlatformWindows == true ? dir.pathname.slice(1) : dir.pathname}`, pathname, `og.png`);
-          fs.writeFileSync(
-            pathFile,
-            file
+          const file = fs.readFileSync(`src/assets/images/og.png`);
+          const pathFile = path.join(
+            `${isPlatformWindows == true ? dir.pathname.slice(1) : dir.pathname}`,
+            pathname,
+            `og.png`
           );
+          fs.writeFileSync(pathFile, file);
         }
       }
 
-
       console.log("satori-og: build finished");
     },
-  }
-})
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), og(), sitemap()],
-  site: 'https://mystica.me',
+  site: "https://mystica.me",
   transitions: true,
   markdown: {
-    remarkPlugins: [
-      remarkReadingTime,
-    ],
+    remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       theme: "slack-dark",
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  trailingSlash: "always"
+  trailingSlash: "always",
 });
